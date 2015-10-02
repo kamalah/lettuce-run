a725425939ab4872473be033f8ac31af1053d335
kamalah <kamalah.chang@gmail.com>
2015-10-02 12:29:19 +0200

    fixed error for cross-train days in compliance check

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -38,27 +38,39 @@ class Plan < ActiveRecord::Base
 	def analyze
 		training_plan = workouts.where(planned: true).group_by(&:date_only)
 		actual_workouts = workouts.where(planned: false).group_by(&:date_only)
-		
-		#training_to_date = (start_date..Date.today).to_a 
-		training_to_date = (start_date..(Date.today+30)).to_a
+		#last_day = Date.today
+		last_day = Date.today + 30 
+		training_to_date = (start_date..last_day).to_a
 		#metrics
 		#compliance[0] - did actual workout on day scheduled
 		#completed actual mileage for the week
 		compliance = Array.new(3,0)
 		training_days = training_to_date.length
+		run_days = training_plan.where("(activity = ?) AND (date < ?)", 'Run', last_day).count
+
 		training_to_date.each do |date|
-			
 			if training_plan[date] 
-				if actual_workouts[date]
-					actual_workouts[date].each do |workout|
-					if (workout.activity == 'Run') 
-						compliance[0] += 0.5 #bonus for running on running day
-					end	
-					converted = workout.convert_to_run
-					compliance[0] += (converted[:distance]/training_plan[date][0].distance) + (converted[:duration]/training_plan[date][0].duration)
+				if (training_plan[date][0].activity == 'Run') #check run day
+					if actual_workouts[date]
+						actual_workouts[date].each do |workout|
+						if (workout.activity == 'Run') 
+							compliance[0] += 0.5 #bonus for running on running day
+						end	
+						converted = workout.convert_to_run
+						compliance[0] += (converted[:distance]/training_plan[date][0].distance) + (converted[:duration]/training_plan[date][0].duration)
+						end
 					end
+				else #check cross-train day
+					if actual_workouts[date]
+						actual_workouts[date].each do |workout|
+							if (workout.activity == 'Run') 
+								compliance[1] += 0.5 #bonus for running on non-running day
+							end	
+							compliance[1] += workout.duration/training_plan[date][0].duration)
+							end
+
 				end	
-			elsif actual_workouts[date]
+			elsif actual_workouts[date] #checks for unplanned workouts
 				
 			end
 		end
