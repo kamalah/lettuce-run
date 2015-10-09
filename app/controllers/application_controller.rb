e9d9b12c09c87ff09cce6861ef1edd01698cd344
kamalah <kamalah.chang@gmail.com>
2015-10-09 15:50:05 +0200

    working version before adding calendar link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     def after_sign_in_path_for(resource)
-    binding.pry
     sign_in_url = new_user_session_url
     if request.referer == sign_in_url
       super
